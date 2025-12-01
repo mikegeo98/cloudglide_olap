a754c140601d456016ed8b144c1462c87fc9413f
Bukii <lukas.weingand@gmail.com>
2025-12-01 02:00:49 +0100

    fix small shell execution bug

--- frontend/lib/exec_shell_cmd.ts
+++ frontend/lib/exec_shell_cmd.ts
@@ -4,7 +4,8 @@ import { InputData } from "@/components/provider";
 import { exec } from "child_process";
 
 export default async function runSimulation(data: InputData) {
-    exec("cd .. && python main.py " + data.architecture + " ./frontend/lib/mixed_arch.json", (err, stdout, stderr) => {
+    const architecture = data.architecture ? data.architecture : "dwaas_case"
+    exec("cd .. && python main.py " + architecture + " ./frontend/lib/mixed_arch.json", (err, stdout, stderr) => {
         if (err) {
             console.error(err)
             return
